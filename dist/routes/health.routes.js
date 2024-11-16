d857bc97e1323f015007f9784f1a2a8741dd827b
Darlan Schwartz <dev@bizuu21.com.br>
2024-11-16 18:41:08 -0300

    Chore: removed dist from commited files

--- dist/routes/health.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.healthRouter = void 0;
-const express_1 = require("express");
-const healthRouter = (0, express_1.Router)();
-exports.healthRouter = healthRouter;
-healthRouter.all('/health', (req, res) => {
-    res.status(200).send('OK');
-});
--- dist/routes/routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mainRouter = void 0;
-const health_routes_1 = require("./health.routes");
-const express_1 = require("express");
-const mainRouter = (0, express_1.Router)();
-exports.mainRouter = mainRouter;
-mainRouter.use(health_routes_1.healthRouter);
